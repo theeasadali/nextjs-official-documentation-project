4411ea3e019e2b0aa07fc98faaf03ddc9708df03
Asad Ali <theeasadali@gmail.com>
2024-08-17 16:06:43 +0500

    Updated paths under baseUrl in ts-config

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { NextFont } from 'next/dist/compiled/@next/font';
 import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
-export const lusitana: NextFont = Lusitana({subsets: ['latin'], weight: ["400", "700"]});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ["400", "700"]});

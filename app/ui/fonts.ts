2bf5bf664738b340725db866415c296d3ae7e925
Asad Ali <theeasadali@gmail.com>
2024-08-17 15:58:26 +0500

    Explicitly added NextFont to Lusitana Font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
+import { NextFont } from 'next/dist/compiled/@next/font';
 import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin']});
-export const lusitana = Lusitana({subsets: ['latin'], weight: ["400", "700"]});
+export const lusitana: NextFont = Lusitana({subsets: ['latin'], weight: ["400", "700"]});
